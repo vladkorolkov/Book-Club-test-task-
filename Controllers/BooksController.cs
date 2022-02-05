8f588cba9a9585ab2b3630663ccc2ffc03e112cb
Vlad <korolkovjazz@yandex.ru>
2022-02-05 12:57:38 +0300

    before session

--- Controllers/BooksController.cs
+++ Controllers/BooksController.cs
@@ -5,23 +5,26 @@ namespace BookClub.Controllers
 {
     public class BooksController : Controller
     {
+        private static List<Book> ReadBooks;
         BooksContext Db;
         public BooksController(BooksContext db)
         {
             Db = db;
+           
         }
-
-        
-        public IActionResult MyBooks()
+        public IActionResult AllBooks()
         {
             var allbooks = Db.Books.ToList();
             return View(allbooks);
         }
 
-        public string MarkAsRead()
+        [HttpPost]
+        public IActionResult MarkAsRead(int id)
         {
-            string test = "marked";
-            return test;
+            
+            var query = Db.Books.Where(i => i.BookId == id).First();
+            ReadBooks.Add(query);
+            return View("ReadBooks", ReadBooks);
         }
 
     }
